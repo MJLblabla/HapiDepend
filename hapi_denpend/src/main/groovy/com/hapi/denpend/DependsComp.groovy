68212005a428a8ce059ed8bfc45fd720658fba7f
manjiale <1013666753@qq.com>
2020-01-15 15:31:04 +0800

    androidx update

--- hapi_denpend/src/main/groovy/com/hapi/denpend/DependsComp.groovy
+++ hapi_denpend/src/main/groovy/com/hapi/denpend/DependsComp.groovy
@@ -1,19 +1,19 @@
 package com.hapi.denpend
 
 public class DependsComp {
-    int compileSdkVersion = 27
-    String buildToolsVersion = '27.0.3'
+    int compileSdkVersion = 28
+    String buildToolsVersion = '28.0.3'
 
     int minSdkVersion = 17
-    int targetSdkVersion = 27
-    String supportVersion = '27.1.1'
+    int targetSdkVersion = 28
+    String androidxVersion = "1.1.0"
+    String designVersion = "1.0.0"
+    String supportVersion = '28.0.0'
 
     boolean latest = true
     boolean thirdPartLatest = false
 
-    // =====================================================================
-    // ========================== 系统库 ====================================
-    // =====================================================================
+
     def support_v4() {
         return support_v4(supportVersion)
     }
@@ -30,68 +30,137 @@ public class DependsComp {
         return "com.android.support:appcompat-v7:${version}"
     }
 
-    def cardview_v7() {
-        return cardview_v7(supportVersion)
+    def appcompat() {
+        return appcompat(androidxVersion)
     }
 
-    def cardview_v7(String version) {
-        return "com.android.support:cardview-v7:${version}"
+    def appcompat(String version) {
+        return "androidx.appcompat:appcompat:${version}"
     }
 
-    def rv_v7() {
-        return rv_v7(supportVersion)
+    def support_compat() {
+        return support_compat("1.2.0-rc01")
     }
 
-    def rv_v7(String version) {
-        return "com.android.support:recyclerview-v7:${version}"
+    def support_compat(String version) {
+        return "androidx.core:core:${version}"
+    }
+
+
+    def cardview() {
+        return cardview(designVersion)
+    }
+
+    def cardview(String version) {
+        return "androidx.cardview:cardview:${version}"
+    }
+
+    def recyclerview() {
+        return recyclerview("1.1.0-rc01")
+    }
+
+    def recyclerview(String version) {
+        return "androidx.recyclerview:recyclerview:${version}"
     }
 
     def design() {
-        return design(supportVersion)
+        return design("1.1.0-beta02")
     }
 
     def design(String version) {
-        return "com.android.support:design:${version}"
+        return "com.google.android.material:material:${version}"
     }
 
     def percent() {
-        return percent(supportVersion)
+        return percent(designVersion)
     }
 
     def percent(String version) {
-        return "com.android.support:percent:${version}"
+        return "androidx.percentlayout:percentlayout:${version}"
     }
 
     def constraintLayout() {
-        String version = latest ? 'latest.release' : '1.0.2'
-        return constraintLayout(version)
+        return constraintLayout("1.1.3")
     }
 
     def constraintLayout(String version) {
-        return "com.android.support.constraint:constraint-layout:${version}"
+        return "androidx.constraintlayout:constraintlayout:${version}"
     }
 
     def multidex() {
-        String version = latest ? 'latest.release' : '1.0.2'
-        return multidex(version)
+        return multidex("2.0.1")
     }
 
     def multidex(String version) {
-        return "com.android.support:multidex:${version}"
+        return "androidx.multidex:multidex:${version}"
+    }
+
+    def fragment() {
+        fragment("1.2.0-rc03")
+    }
+
+    def fragment(String version) {
+        return "androidx.fragment:fragment:${version}"
+    }
+
+    def viewpager() {
+        viewpager(designVersion)
+    }
+
+    def viewpager(String version) {
+        return "androidx.viewpager:viewpager:${version}"
+    }
+
+    def coordinatorlayout() {
+        coordinatorlayout(androidxVersion)
+    }
+
+    def coordinatorlayout(String version) {
+        return "androidx.coordinatorlayout:coordinatorlayout:${version}"
+    }
+
+    def drawerlayout() {
+        drawerlayout(designVersion)
+    }
+
+    def drawerlayout(String version) {
+        return "androidx.drawerlayout:drawerlayout:${version}"
+    }
+
+    def gridlayout() {
+        gridlayout(androidxVersion)
+    }
+
+    def gridlayout(String version) {
+        return "androidx.gridlayout:gridlayout:${version}"
+    }
+
+    def annotations() {
+        annotations(androidxVersion)
+    }
+
+    def annotations(String version) {
+        return "androidx.annotation:annotation:${version}"
     }
 
     def lifecycle() {
-        String version = latest ? 'latest.release' : '1.1.1'
-        return lifecycle(version)
+        return lifecycle("2.1.0")
     }
 
     def lifecycle(String version) {
-        return "android.arch.lifecycle:extensions:${version}"
+        return "androidx.lifecycle:lifecycle-extensions:${version}"
+    }
+
+    def swiperefreshlayout() {
+        swiperefreshlayout(designVersion)
+    }
+
+    def swiperefreshlayout(String version) {
+        return "androidx.swiperefreshlayout:swiperefreshlayout:${version}"
     }
 
     def kotlin() {
-        String version = latest ? 'latest.release' : '1.2.50'
-        return kotlin(version)
+        return kotlin("1.3.61")
     }
 
     def kotlin(String version) {
@@ -112,13 +181,21 @@ public class DependsComp {
      * @return
      */
     def kapt_databinding() {
-        String version = latest ? 'latest.release' : '3.1.0'
-        return kotlin_anko(version)
+        return kapt_databinding("3.5.3")
     }
 
     def kapt_databinding(String version) {
-        return "'com.android.databinding:compiler:${version}"
+        return "androidx.databinding:databinding-compiler:${version}"
     }
+
+    def databinding() {
+        return databinding("3.5.3")
+    }
+
+    def databinding(String version) {
+        return "androidx.databinding :databinding-common:${version}"
+    }
+
 
     // =====================================================================
     // ========================== 底层lib库 =================================
